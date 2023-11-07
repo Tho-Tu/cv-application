9ba7a5a787cf9b2ac6e0feae0e5f058d0e5b59fe
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-11-07 18:39:08 +1000

    Ensure responsive design

--- src/components/cv-form/GeneralInfo.tsx
+++ src/components/cv-form/GeneralInfo.tsx
@@ -1,19 +1,69 @@
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 
 export default function GeneralInfo() {
-  const [text, setText] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setphoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleFullName = (e: { target: { value: SetStateAction<string> } }) => {
+    setFullName(e.target.value);
+  };
+
+  const handleEmail = (e: { target: { value: SetStateAction<string> } }) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePhoneNumber = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setphoneNumber(e.target.value);
+  };
+
+  const handleAddress = (e: { target: { value: SetStateAction<string> } }) => {
+    setAddress(e.target.value);
+  };
   return (
     <div className="general-info">
       <h2>General Info</h2>
       <form>
-        <label>Full Name</label>
-        <input placeholder="First and Last name"></input>
-        <label>Email</label>
-        <input placeholder="Email"></input>
-        <label>Phone number</label>
-        <input placeholder="Phone number"></input>
-        <label>Address</label>
-        <input placeholder="Address"></input>
+        <label>
+          Full Name
+          <input
+            type="text"
+            placeholder="First and Last name"
+            value={fullName}
+            onChange={handleFullName}
+          ></input>
+        </label>
+        <label>
+          Email
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmail}
+          ></input>
+        </label>
+        <label>
+          Phone number
+          <input
+            type="text"
+            placeholder="Phone number"
+            value={phoneNumber}
+            onChange={handlePhoneNumber}
+          ></input>
+        </label>
+        <label>
+          Address
+          <input
+            type="text"
+            placeholder="Address"
+            value={address}
+            onChange={handleAddress}
+          ></input>
+        </label>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
