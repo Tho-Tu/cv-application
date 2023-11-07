09dd3cd39eea80f2bea9d029034267d9c39cb595
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-11-07 11:15:09 +1000

    Add form and preview components

---
+++ src/components/cv-form/Education.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default function Education() {
+  const [text, setText] = useState("");
+  return (
+    <>
+      <form></form>
+    </>
+  );
+}
---
+++ src/components/cv-preview/PreviewEducation.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default function PreviewEducation() {
+  const [text, setText] = useState("");
+  return (
+    <>
+      <form></form>
+    </>
+  );
+}
