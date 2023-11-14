263cc7eaa64f38988a496d83c2c36030e068214e
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-11-14 16:38:36 +1000

    Ensure education button adds empty object to list
    Need to fix save and delete button for individual object

--- src/components/cv-form/Education.tsx
+++ src/components/cv-form/Education.tsx
@@ -3,40 +3,96 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function Education() {
   const [educationList, setEducationList] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  const handleButton = () => {
+  const handleSaveButton = (educationObject) => {
+    const newEducation = { ...educationObject };
     setEducationList((prevEducationList) => [
       ...prevEducationList,
-      emptyEducationFormFactory(),
+      newEducation,
     ]);
+
     console.log(educationList);
   };
+
+  const handleDeleteButton = () => {
+    const newEducationList = educationList.splice(activeIndex, 1);
+    setEducationList(newEducationList);
+
+    console.log(educationList);
+  };
+
+  const handleNewButton = () => {
+    setEducationList((prevEducationList) => [
+      ...prevEducationList,
+      educationFormFactory(),
+    ]);
+
+    console.log(educationList);
+  };
+
   return (
     <div className="education">
       <h2>Education</h2>
-
-      <SingleEducation />
-
-      <button type="button" onClick={handleButton}>
+      <EducationListComponent
+        educationList={educationList}
+        saveButton={() => handleSaveButton(educationFormFactory())}
+        deleteButton={handleDeleteButton}
+      />
+      <button type="button" onClick={handleNewButton}>
         + Education
       </button>
     </div>
   );
 }
 
-function emptyEducationFormFactory() {
+function EducationListComponent({ educationList, saveButton, deleteButton }) {
+  if (educationList.length === 0) {
+    return <></>;
+  }
+
+  return educationList.map((education) => {
+    if (education.visible === true) {
+      return (
+        <EducationForm
+          key={education.id}
+          saveButton={saveButton}
+          deleteButton={deleteButton}
+        />
+      );
+    }
+  });
+}
+
+function educationFormFactory() {
   const school = "";
   const degree = "";
   const startDate = "";
   const endDate = "";
   const location = "";
-  const visible = false;
+  const visible = true;
   const id = uuidv4();
 
   return { school, degree, startDate, endDate, location, visible, id };
 }
 
-function SingleEducation() {
+function LabelComponent({ name, value, handlerFunction }) {
+  return (
+    <>
+      <label>
+        {name}
+        <input
+          type="text"
+          placeholder={name}
+          value={value}
+          onChange={handlerFunction}
+        ></input>
+      </label>
+    </>
+  );
+}
+
+function EducationForm({ saveButton, deleteButton }) {
   const [school, setSchool] = useState("");
   const [degree, setDegree] = useState("");
   const [startDate, setStartDate] = useState("");
@@ -67,52 +123,40 @@ function SingleEducation() {
 
   return (
     <>
-      <form>
-        <label>
-          School
-          <input
-            type="text"
-            placeholder="School"
-            value={school}
-            onChange={handleSchool}
-          ></input>
-        </label>
-        <label>
-          Degree
-          <input
-            type="text"
-            placeholder="Degree"
-            value={degree}
-            onChange={handleDegree}
-          ></input>
-        </label>
-        <label>
-          Start Date
-          <input
-            type="text"
-            placeholder="Start Date"
-            value={startDate}
-            onChange={handleStartDate}
-          ></input>
-        </label>
-        <label>
-          End Date
-          <input
-            type="text"
-            placeholder="End Date"
-            value={endDate}
-            onChange={handleEndDate}
-          ></input>
-        </label>
-        <label>
-          Location
-          <input
-            type="text"
-            placeholder="End Date"
-            value={location}
-            onChange={handleLocation}
-          ></input>
-        </label>
+      <form className="forms">
+        <LabelComponent
+          name="School"
+          value={school}
+          handlerFunction={handleSchool}
+        />
+        <LabelComponent
+          name="Degree"
+          value={degree}
+          handlerFunction={handleDegree}
+        />
+        <LabelComponent
+          name="Start Date"
+          value={startDate}
+          handlerFunction={handleStartDate}
+        />
+        <LabelComponent
+          name="End Date"
+          value={endDate}
+          handlerFunction={handleEndDate}
+        />
+        <LabelComponent
+          name="Location"
+          value={location}
+          handlerFunction={handleLocation}
+        />
+        <span>
+          <button type="button" onClick={saveButton}>
+            Save
+          </button>{" "}
+          <button type="button" onClick={deleteButton}>
+            Delete
+          </button>{" "}
+        </span>
       </form>
     </>
   );
