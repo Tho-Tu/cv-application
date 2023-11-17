2c46c3d89a49838748967217e67c5a84b466c059
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-11-17 19:19:03 +1000

    Update styles for preview experience section

--- src/components/cv-preview/PreviewExperience.tsx
+++ src/components/cv-preview/PreviewExperience.tsx
@@ -5,16 +5,22 @@ export default function PreviewExperience({ experienceInfo }) {
   return (
     <div className="preview-experience">
       <h2>Experience</h2>
-      <div className="experience-company-name">Google</div>
-      <div className="experience-position-title">Software Engineer, Intern</div>
-      <div className="experience-start-date">08/2022 -</div>
-      <div className="experience-end-date">present</div>
-      <div className="experience-location">New York City, US</div>
-      <div className="experience-description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere placeat
-        nulla ea possimus at sapiente magni voluptatem enim cupiditate fugiat,
-        aspernatur quam distinctio blanditiis vero obcaecati perspiciatis animi
-        consectetur quos!
+      <div className="single-experience">
+        <div className="experience-start-date">
+          08/2022 - <span className="experience-end-date">present</span>
+        </div>
+        <div className="experience-company-name">Google</div>
+        <div className="experience-location">New York City, US</div>
+
+        <div className="experience-position-title">
+          Software Engineer, Intern
+        </div>
+        <div className="experience-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+          placeat nulla ea possimus at sapiente magni voluptatem enim cupiditate
+          fugiat, aspernatur quam distinctio blanditiis vero obcaecati
+          perspiciatis animi consectetur quos!
+        </div>
       </div>
     </div>
   );
